1fbc74c9d1a52944cd2f69cb58ec40a78db641dd
Michael Balders <michael.balders@taxi-nyc.com>
2018-01-11 15:38:48 -0500

    inital build

--- index.js
+++ index.js
@@ -3,44 +3,64 @@
 const neeoapi = require('neeo-sdk');
 const controller = require('./controller');
 
-console.log('NEEO SDK Example "simpleCustomDevice" adapter');
+const NEEO_PORT = 6336;
+
+console.log('NEEO Driver for Oppo UDP-203 and 205');
 console.log('---------------------------------------------');
 
-/*
- * Adapter - an Adapter contains one or more DEVICES. In this case we only use a single very
- * simple device.
- */
+const oppoUdp = neeoapi.buildDevice('OPPO UDP-20X IP')
+.setManufacturer('OPPO')
+.addAdditionalSearchToken('UDP-203')
+.addAdditionalSearchToken('UDP-205')
+.setType('MEDIAPLAYER')
 
-// first we set the device info, used to identify it on the Brain
-const customLightDevice = neeoapi.buildDevice('OPPO UDP-20X IP')
-  .setManufacturer('OPPO')
-  .addAdditionalSearchToken('UDP-203')
-  .addAdditionalSearchToken('UDP-205')
-  .setType('MEDIAPLAYER')
+.addButtonGroup('POWER')
+.addButtonGroup('VOLUME')
+.addButtonGroup('Numpad')
+.addButtonGroup('Controlpad')
+.addButtonGroup('Color Buttons')
+.addButtonGroup('Menu and Back')
+.addButtonGroup('Channel Zapper')
+.addButtonGroup('Transport')
+.addButtonGroup('Transport Search')
+.addButtonGroup('Transport Scan')
 
-  .addButtonGroup('Power')
-  .addButtonGroup('Menu and Back')
-  .addButtonGroup('Controlpad')
-  .addButtonGroup('Transport')
-  .addButtonGroup('Transport Search')
-  .addButtonGroup('Transport Scan')
-  .addButtonGroup('Color Buttons')
-  .addButton({ name: 'HOME MENU', label: 'Home' })
-  .addButton({ name: 'SETUP', label: 'Setup' })
-  .addButton({ name: 'AUDIO', label: 'Audio' })
-  .addButtonGroup('Channel Zapper')
-  .addButton({ name: 'DIRECT', label: 'Source Direct' })
-  .addButton({ name: '1080P', label: '1080p' })
-  
-  .addButtonHander(controller.onButtonPressed);
+.addButton({ name: 'PURE AUDIO', label: 'Pure Audio' })
+
+.addButton({ name: 'CLEAR', label: 'Clear' })
+.addButton({ name: 'GOTO', label: 'Go To' })
+
+.addButton({ name: 'TOP MENU', label: 'Top Menu' })
+.addButton({ name: 'OPTION', label: 'Option' })
+
+.addButton({ name: 'HOME MENU', label: 'Home Menu' })
+.addButton({ name: 'OPEN', label: 'Open' })
+.addButton({ name: 'DIMMER', label: 'Dimmer' })
+
+.addButton({ name: 'INFO', label: 'Info' })
+.addButton({ name: 'SETUP', label: 'Setup' })
+.addButton({ name: 'PIC', label: 'Pic' })
+.addButton({ name: 'RESOLUTION', label: 'Resolution' })
+
+.addButton({ name: 'AUDIO', label: 'Sutio' })
+.addButton({ name: 'SUBTITLE', label: 'Subtitle' })
+.addButton({ name: 'ZOOM', label: 'Zoom' })
+.addButton({ name: 'AB REPLAY', label: 'AB Replay' })
+.addButton({ name: 'REPEAT', label: 'Repeat' })
+
+.addButton({ name: 'INPUT', label: 'Input' })
+.addButton({ name: 'HDR', label: 'HDR' })
+
+.addButtonHander(controller.onButtonPressed);
+
 
 function startSdkExample(brain) {
   console.log('- Start server');
   neeoapi.startServer({
     brain,
-    port: 6336,
-    name: 'simple-adapter-one',
-    devices: [customLightDevice]
+    port: NEEO_PORT,
+    name: 'oppo-udp-20x',
+    devices: [oppoUdp]
   })
   .then(() => {
     console.log('# READY! use the NEEO app to search for "NEEO Simple Device".');
@@ -52,8 +72,7 @@ function startSdkExample(brain) {
   });
 }
 
-//const brainIp = process.env.BRAINIP;
-const brainIp = '10.0.1.235';
+const brainIp = process.env.BRAINIP;
 if (brainIp) {
   console.log('- use NEEO Brain IP from env variable', brainIp);
   startSdkExample(brainIp);
